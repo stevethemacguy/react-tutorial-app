9f0efbc0a9b8d2c13a3caf220c11717e6557b554
Steven Dunn <Dunn.Steven1@gmail.com>
2021-05-24 22:31:06 -0700

    The game now calculates a winner.

--- src/index.js
+++ src/index.js
@@ -30,7 +30,14 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    let status; // This is initialized immediately below so there' no need to initialize it
+
+    let winner = calculateWinner(this.state.squares); // check for a winner
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -71,9 +78,36 @@ class Game extends React.Component {
   }
 }
 
+// Given an array of 9 squares, this function checks for a winner and returns 'X', 'O', or null as appropriate.
+function calculateWinner(squares) {
+  // All possible Winning conditions for either the X player or O player
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  // For each possible winning condition...
+  for (let i = 0; i < lines.length; i++) {
+    // Check one winning condition
+    const [a, b, c] = lines[i];
+    // Check the values of each square at the 'winning' array index combination. If the values in each square
+    // match each other (i.e. all 'X' or all 'O'), then return the winner (e.g. 'X', 'O')
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  // If no winner was found, just return null;
+  return null;
+}
 // ========================================
 
 ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
